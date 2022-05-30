4c8d80bc7731a495859b7e584aeffde258af7375
JonatanSegal <82033910+JonatanSegal@users.noreply.github.com>
2022-05-30 13:23:11 +0200

    Removed SpringBootTest

--- src/test/java/com/example/exambackend/ExamBackendApplicationTests.java
+++ src/test/java/com/example/exambackend/ExamBackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.exambackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ExamBackendApplicationTests {
 
     @Test
